52259b49a1394264da7f6538f6d80c462a04f000
MarceloFreitas <marcelohfreitas235@gmail.com>
2024-07-01 20:48:39 -0300

    Add: migrations, models e controllers iniciais de hotels & rooms

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -26,8 +26,7 @@ const submit = () => {
         <Head title="Forgot Password" />
 
         <div class="mb-4 text-sm text-gray-600">
-            Forgot your password? No problem. Just let us know your email address and we will email you a password reset
-            link that will allow you to choose a new one.
+            Esqueceu a sua senha? Sem problemas! Preencha o campo abaixo com o e-mail registrado para enviar a redefinição de senha.
         </div>
 
         <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
@@ -53,7 +52,7 @@ const submit = () => {
 
             <div class="flex items-center justify-end mt-4">
                 <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Email Password Reset Link
+                    Enviar Redefinição de senha
                 </PrimaryButton>
             </div>
         </form>

b91bc61466f476ca16ef029e6fa98b770531ea40
MarceloFreitas <marcelohfreitas235@gmail.com>
2024-07-03 09:11:27 -0300

    Add: logo de hotel, retirada padrão do laravel, alterada rota dashboard para index, retirada rota welcome

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -20,7 +20,7 @@ const showingNavigationDropdown = ref(false);
                         <div class="flex">
                             <!-- Logo -->
                             <div class="shrink-0 flex items-center">
-                                <Link :href="route('dashboard')">
+                                <Link :href="route('index')">
                                     <ApplicationLogo
                                         class="block h-9 w-auto fill-current text-gray-800"
                                     />
@@ -29,7 +29,7 @@ const showingNavigationDropdown = ref(false);
 
                             <!-- Navigation Links -->
                             <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
+                                <NavLink :href="route('index')" :active="route().current('index')">
                                     Index
                                 </NavLink>
                                 <NavLink :href="route('hotels.index')" :active="route().current('hotels.*')">
@@ -119,8 +119,14 @@ const showingNavigationDropdown = ref(false);
                     class="sm:hidden"
                 >
                     <div class="pt-2 pb-3 space-y-1">
-                        <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                            Dashboard
+                        <ResponsiveNavLink :href="route('index')" :active="route().current('index')">
+                            Index
+                        </ResponsiveNavLink>
+                        <ResponsiveNavLink :href="route('hotels.index')" :active="route().current('hotels.*')">
+                            Hotels
+                        </ResponsiveNavLink>
+                        <ResponsiveNavLink :href="route('rooms.index')" :active="route().current('rooms.*')">
+                            Rooms
                         </ResponsiveNavLink>
                     </div>
 

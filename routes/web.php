c71d2084128b56204118e02c2e454bb33ce60344
MarceloFreitas <marcelohfreitas235@gmail.com>
2024-07-03 10:56:58 -0300

    Fix: teste unitário em redirecionamento para rota raíz /

--- routes/web.php
+++ routes/web.php
@@ -7,22 +7,24 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return redirect()->route('login');
-});
 
-Route::get('/index', function () {
-    return Inertia::render('Index');
-})->middleware(['auth', 'verified'])->name('index');
 
 Route::middleware('auth')->group(function () {
+
+
+    Route::get('/', function () {
+        return Inertia::render('Index');
+    })->name('index');
+
+
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
 
-    Route::resource('hotels',HotelController::class);
-    Route::resource('rooms',RoomController::class);
+    Route::resource('hotels', HotelController::class);
+    Route::resource('rooms', RoomController::class);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,18 +2,40 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
-     * A basic test example.
+     * Testa o redirecionamento para o login quando não autenticado.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_redirect_to_login_when_not_authenticated(): void
     {
+        // Faz a requisição GET para '/'
         $response = $this->get('/');
 
+        // Verifica se houve redirecionamento para a rota de login
+        $response->assertStatus(302); // Deve redirecionar para /login
+        $response->assertRedirect('/login');
+    }
+
+    /**
+     * Testa se a aplicação retorna uma resposta bem-sucedida quando autenticado.
+     */
+    public function test_successful_response_when_authenticated(): void
+    {
+        // Cria um usuário e autentica
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        // Faz a requisição GET para '/'
+        $response = $this->get('/');
+
+        // Verifica se a resposta tem status 200 (bem-sucedida)
         $response->assertStatus(200);
     }
 }
